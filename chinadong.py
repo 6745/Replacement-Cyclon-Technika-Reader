e61006f914b42deab0087bfaab3e2374c4241396
7645 <75388599+6745@users.noreply.github.com>
2023-03-01 04:40:04 +0000

    a

--- chinadong.py
+++ chinadong.py
@@ -17,20 +17,16 @@ parser.add_argument("BAUD", type=int, help="Baud rate")
 args = parser.parse_args()
 
 ser = serial.Serial(args.COM, args.BAUD)
-ignore_list = ["Error in communication", "Authentication failed", "Timeout in communication"]
+ignore_list = ["Error in communication", "Authentication failed", "Timeout in communication","The CRC_A does not match."]
 
 while True:
     data = ser.readline().decode('utf-8').strip()
     if data.startswith("Name: "):
         name = data
-        debug = data.replace("Name: ", "DEBUG: ").replace("\x00", "").replace("Error in communication", "BLOCK READ FAILED").replace("Timeout in communication", "BLOCK READ TIMEOUT")
+        debug = data.replace("Name: ", "DEBUG: ").replace("\x00", "").replace("Error in communication", "BLOCK READ FAILED").replace("Timeout in communication", "BLOCK READ TIMEOUT",).replace("The CRC_A does not match.","CRC FAIL")
         print(debug) #debug 
         if not any(ignore_string in name for ignore_string in ignore_list):
             name = data[22:].replace(" ", "").replace("\x00", "")
-            if "CRC_A" in name:
-                print("DEBUG: CRC check failed. Restarting script.")
-                python = sys.executable
-                os.execl(python, python, *sys.argv)
             if len(name) == 20:
                 print("DEBUG: Block Read Success! Saving to file..") #debug 
                 with open("CardNum.txt", "w") as file:
