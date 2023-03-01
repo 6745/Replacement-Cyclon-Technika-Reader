0dfb88840963b6793212f431098ab6d75b937e2e
7645 <75388599+6745@users.noreply.github.com>
2023-03-01 04:32:12 +0000

    fix?

--- chinadong.py
+++ chinadong.py
@@ -1,12 +1,16 @@
 import serial
 import time
 import argparse
-print( " ██████ ██████  ████████ ██████      ██    ██ ██████  ")
-print( "██      ██   ██    ██    ██   ██     ██    ██      ██ ")
-print( "██      ██   ██    ██    ██████      ██    ██  █████  ")
-print( "██      ██   ██    ██    ██   ██      ██  ██  ██      ")
-print( " ██████ ██████     ██    ██   ██       ████   ███████ ")
-print( "By Github.com/6745")
+import os
+import sys
+
+print(" ██████ ██████  ████████ ██████      ██    ██ ██████  ")
+print("██      ██   ██    ██    ██   ██     ██    ██      ██ ")
+print("██      ██   ██    ██    ██████      ██    ██  █████  ")
+print("██      ██   ██    ██    ██   ██      ██  ██  ██      ")
+print(" ██████ ██████     ██    ██   ██       ████   ███████ ")
+print("By Github.com/6745")
+
 parser = argparse.ArgumentParser()
 parser.add_argument("COM", help="Serial port identifier")
 parser.add_argument("BAUD", type=int, help="Baud rate")
@@ -14,17 +18,19 @@ args = parser.parse_args()
 
 ser = serial.Serial(args.COM, args.BAUD)
 ignore_list = ["Error in communication", "Authentication failed", "Timeout in communication"]
+
 while True:
     data = ser.readline().decode('utf-8').strip()
     if data.startswith("Name: "):
         name = data
-        debug = data.replace("Name: ", "DEBUG: ").replace("\x00", "").replace("Error in communication","BLOCK READ FAILED").replace("Timeout in communication","BLOCK READ TIMEOUT")
+        debug = data.replace("Name: ", "DEBUG: ").replace("\x00", "").replace("Error in communication", "BLOCK READ FAILED").replace("Timeout in communication", "BLOCK READ TIMEOUT")
         print(debug) #debug 
         if not any(ignore_string in name for ignore_string in ignore_list):
             name = data[22:].replace(" ", "").replace("\x00", "")
-            if name == "The CRC_A does not match.":
-                print("DEBUG: CRC check failed. Restarting loop.")
-                continue
+            if "CRC_A" in name:
+                print("DEBUG: CRC check failed. Restarting script.")
+                python = sys.executable
+                os.execl(python, python, *sys.argv)
             if len(name) == 20:
                 print("DEBUG: Block Read Success! Saving to file..") #debug 
                 with open("CardNum.txt", "w") as file:
